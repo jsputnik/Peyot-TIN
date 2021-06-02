754cd93ad4e5ac7375f900de9f5605ebd7c1f1f8
SzymonKisiel <szymon.kisiel99@gmail.com>
2021-06-02 22:35:18 +0200

    Date class improvements

--- server/database/Date.cpp
+++ server/database/Date.cpp
@@ -1,5 +1,52 @@
-//
-// Created by szymon on 6/2/21.
-//
+//02.06.2021
 
 #include "Date.h"
+
+Date::Date() : start_time(std::time(nullptr)), end_time(std::time(nullptr))  {
+}
+
+void Date::set_start(std::string date) {
+    time_t now = std::time(nullptr);
+    struct std::tm t_now = *std::localtime(&now);
+
+    struct std::tm tm;
+    std::istringstream ss("14.05.2021 16:31");
+    ss >> std::get_time(&tm, "%d.%m.%Y %H:%M");
+    if (ss.fail()) {
+        std::cerr << "Date::set_start error - wrong date format\n";
+        return;
+    }
+    tm.tm_zone = t_now.tm_zone;
+    tm.tm_gmtoff = t_now.tm_gmtoff;
+    tm.tm_isdst = t_now.tm_isdst;
+    start_time = mktime(&tm);
+}
+
+void Date::set_end(std::string date) {
+    time_t now = std::time(nullptr);
+    struct std::tm t_now = *std::localtime(&now);
+
+    struct std::tm tm;
+    std::istringstream ss(date);
+    ss >> std::get_time(&tm, "%d.%m.%Y %H:%M");
+    if (ss.fail()) {
+        std::cerr << "Date::set_start error - wrong date format\n";
+        return;
+    }
+    tm.tm_zone = t_now.tm_zone;
+    tm.tm_gmtoff = t_now.tm_gmtoff;
+    tm.tm_isdst = t_now.tm_isdst;
+    end_time = mktime(&tm);
+}
+
+void Date::print() {
+    struct std::tm tm;
+
+    tm = *std::localtime(&start_time);
+    std::cout << "Start: " << std::put_time(&tm, "%d.%m.%Y %H:%M")  << std::endl;
+    //std::cout << std::put_time(&tm, "%c")  << std::endl;
+
+    tm = *std::localtime(&end_time);
+    std::cout << "End: " << std::put_time(&tm, "%d.%m.%Y %H:%M")  << std::endl;
+    //std::cout << std::put_time(&tm, "%c")  << std::endl;
+}
--- server/database/Date.h
+++ server/database/Date.h
@@ -14,25 +14,10 @@ class Date {
     std::time_t start_time;
     std::time_t end_time;
 public:
-    Date() : start_time(std::time(nullptr)), end_time(std::time(nullptr))  {
-    }
-    void print() {
-        struct std::tm tm;
-
-        tm = *std::localtime(&start_time);
-        std::cout << std::put_time(&tm, "%d.%m.%Y %H:%M")  << std::endl;
-        //std::cout << std::put_time(&tm, "%c")  << std::endl;
-
-        tm = *std::localtime(&end_time);
-        std::cout << std::put_time(&tm, "%d.%m.%Y %H:%M")  << std::endl;
-        //std::cout << std::put_time(&tm, "%c")  << std::endl;
-    }
-    void set_time(/*int h, int m, int s*/) {
-        struct std::tm tm;
-        std::istringstream ss("16.05.2021 16:31");
-        ss >> std::get_time(&tm, "%d.%m.%Y %H:%M");
-        start_time = mktime(&tm);
-    }
+    Date();
+    void set_start(std::string date);
+    void set_end(std::string date);
+    void print();
 };
 
 

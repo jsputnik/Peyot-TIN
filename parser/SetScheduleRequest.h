34b36b9b0d13ec7a49588df10694f2e864fdcb03
Iwo Sokal <iwosokal@gmail.com>
2021-06-06 15:26:39 -0700

    set schedule

--- parser/SetScheduleRequest.h
+++ parser/SetScheduleRequest.h
@@ -9,6 +9,27 @@
 class SetScheduleRequest: public Request {
     std::string keyword;
     std::string date;
+
+public:
+    SetScheduleRequest(std::string keyword, std::string date): keyword(keyword), date(date) {
+        this->type = RequestType::SETSCHEDULE;
+    }
+
+    const std::string &getKeyword() const {
+        return keyword;
+    }
+
+    void setKeyword(const std::string &keyword) {
+        SetScheduleRequest::keyword = keyword;
+    }
+
+    const std::string &getDate() const {
+        return date;
+    }
+
+    void setDate(const std::string &date) {
+        SetScheduleRequest::date = date;
+    }
 };
 
 
